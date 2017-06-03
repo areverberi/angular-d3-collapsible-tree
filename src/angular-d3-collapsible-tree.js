f0ba18584faa46d09075b037327b2d58c80fa5ff
Andrea Reverberi <andrea.reverberi@gatech.edu>
2017-06-03 16:35:12 +0200

    width and height resize on tree changes

--- src/angular-d3-collapsible-tree.js
+++ src/angular-d3-collapsible-tree.js
@@ -16,6 +16,7 @@
  			radius: '=',
  			nodes: '=',
  			onNodeClick: '&',
+ 			images: '=',
  		},
  		link: function( scope, element ) {
  			var m = [ 20, 120, 20, 120 ],
@@ -24,7 +25,6 @@
  				i = 0,
  				root;
  			var radius = scope.radius || 18;
-
  			var tree = d3.layout.tree()
  				.size( [ h, w ] );
 
@@ -33,9 +33,30 @@
  					return [ d.y, d.x ];
  				} );
 
- 			var vis = d3.select( "collapsible-tree" ).append( "svg:svg" )
+ 			var svg = d3.select( "collapsible-tree" ).append( "svg:svg" )
  				.attr( "width", scope.width )
- 				.attr( "height", scope.height )
+ 				.attr( "height", scope.height );
+ 			var defs = svg;
+ 			if ( scope.images ) {
+ 				defs = svg.append( 'svg:defs' );
+ 				for ( var img in scope.images ) {
+ 					defs.append( 'svg:pattern' )
+ 						.attr( 'id', img )
+ 						.attr( 'patternContentUnits', 'objectBoundingBox' )
+ 						.attr( 'width', 1 )
+ 						.attr( 'height', 1 )
+ 						.append( 'svg:image' )
+ 						.attr( 'xlink:href', scope.images[ img ] )
+ 						.attr( 'x', 0 )
+ 						.attr( 'y', 0 )
+ 						.attr( 'width', 1 )
+ 						.attr( 'height', 1 )
+ 						.attr( "preserveAspectRatio", "xMinYMin slice" );
+ 				}
+ 			}
+
+
+ 			var vis = defs
  				.append( "svg:g" )
  				.attr( "transform", "translate(" + m[ 3 ] + "," + m[ 0 ] + ")" );
 
@@ -65,7 +86,35 @@
 
  			function update( source ) {
  				var duration = d3.event && d3.event.altKey ? 5000 : 500;
+ 				var levelWidth = [ 1 ];
+ 				var childCount = function( level, n ) {
 
+ 					if ( n.children && n.children.length > 0 ) {
+ 						if ( levelWidth.length <= level + 1 ) levelWidth.push( 0 );
+
+ 						levelWidth[ level + 1 ] += n.children.length;
+ 						n.children.forEach( function( d ) {
+ 							childCount( level + 1, d );
+ 						} );
+ 					}
+ 				};
+ 				childCount( 0, root );
+ 				var newHeight = d3.max( levelWidth ) * 150; // 20 pixels per line
+ 				var maxDepth = 0;
+ 				var depthCount = function( node ) {
+ 					if ( !node.children ) {
+ 						return 1;
+ 					}
+ 					return 1 + d3.max( node.children.map( depthCount ) );
+ 				};
+ 				maxDepth = depthCount( root );
+ 				var newWidth = maxDepth * 200;
+ 				// update the tree height
+ 				tree = tree.size( [ newHeight, newWidth ] );
+
+ 				// >>> UPDATE THE SVG AS WELL <<<
+ 				svg.attr( "height", newHeight + 20 );
+ 				svg.attr( "width", newWidth + 20 );
  				// Compute the new tree layout.
  				var nodes = tree.nodes( root ).reverse();
 
@@ -100,7 +149,14 @@
  				nodeEnter.append( "svg:circle" )
  					.attr( "r", 1e-5 )
  					.style( "fill", function( d ) {
- 						return d._children ? "lightsteelblue" : "#fff";
+ 						if ( scope.images && d.name ) {
+ 							console.info( 'd', d );
+ 							console.info( 'url(#' + d.name.replace( /\s/g, '' ) + ')' );
+ 							return 'url(#' + d.name.replace( /\s/g, '' ) + ')';
+ 						} else {
+ 							return d._children ? "lightsteelblue" : "#fff";
+ 						}
+
  					} );
 
  				nodeEnter.append( "svg:text" )
@@ -123,7 +179,12 @@
  				nodeUpdate.select( "circle" )
  					.attr( "r", radius )
  					.style( "fill", function( d ) {
- 						return d._children ? "lightsteelblue" : "#fff";
+ 						if ( scope.images && d.name ) {
+ 							console.info( 'url(#' + d.name.replace( /\s/g, '' ) + ')' );
+ 							return 'url(#' + d.name.replace( /\s/g, '' ) + ')';
+ 						} else {
+ 							return d._children ? "lightsteelblue" : "#fff";
+ 						}
  					} );
 
  				nodeUpdate.select( "text" )
